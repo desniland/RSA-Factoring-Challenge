60e7e71998c2166d1046f95432ae73bb69a3fd07
desniland <albertdesmond26@gmail.com>
2024-01-27 13:18:18 +0300

    Coding Style Check on Function Files

--- function2.c
+++ function2.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include "factorize_num.h"
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-		return 1;
-	}
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+return (1);
+}
 
-	FILE *fp = fopen(argv[1], "r");
-	if (fp == NULL) {
-		perror("fopen");
-		return 1;
-	}
+FILE *fp = fopen(argv[1], "r");
+if (fp == NULL)
+{
+perror("fopen");
+return (1);
+}
 
-	long int num;
-	while (fscanf(fp, "%ld", &num) == 1) {
-		test_divisor(num);
-	}
+long int num;
+while (fscanf(fp, "%ld", &num) == 1)
+{
+test_divisor(num);
+}
 
-	fclose(fp);
+fclose(fp);
 
-	return 0;
+return (0);
 }
